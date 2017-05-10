8e09d49bfa860302ce01582d2a43291ea2223f8b
Raly Tan <tan_raly@hotmail.com>
2017-05-09 23:50:26 -0700

    Added a class for a Player object

---
+++ Player.java
@@ -0,0 +1,39 @@
+package Lab11;
+
+//*********************************************************
+//Player.java
+//
+//Defines a Player class that holds information about an athlete.
+//**********************************************************
+import java.util.Scanner;
+
+public class Player {
+    
+    private String name;
+    
+    private String team;
+    
+    private int jerseyNumber;
+    //-----------------------------------------------------------
+    //Prompts for and reads in the player's name, team, and
+    //jersey number.
+    //-----------------------------------------------------------
+    public void readPlayer() {
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Name: ");
+        name = scan.nextLine();
+        System.out.print("Team: ");
+        team = scan.nextLine();
+        System.out.print("Jersey number: ");
+        jerseyNumber = scan.nextInt();
+    }
+    
+    public boolean equals(Player obj2) {
+        if (this.team.equals(obj2.team) && this.jerseyNumber == obj2.jerseyNumber) {
+            return true;
+        } else {
+            return false;
+    }
+    }
+}
